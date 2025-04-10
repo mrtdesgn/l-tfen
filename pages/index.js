b056cc0ef2b85a2b73ba5d211e6693c2648eccf8
mrtdesgn <mbalkarkaya5@gmail.com>
2025-04-10 18:24:59 +0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+
 export default function Home() {
   return (
-    <div style={{ fontFamily: 'Arial', padding: 40 }}>
-      <h1>MRT LUXURY INVESTMENTS</h1>
-      <p>Welcome to your real estate investment hub.</p>
+    <div className="min-h-screen flex items-center justify-center text-center">
+      <div>
+        <h1 className="text-4xl font-bold">MRT LUXURY INVESTMENTS</h1>
+        <p className="mt-4 text-lg">Welcome to your real estate investment hub.</p>
+      </div>
     </div>
-  );
+  )
 }
