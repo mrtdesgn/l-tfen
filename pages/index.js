04d44d6b1b978e563f6c779aaf2a61f3a7efcf72
mrtdesgn <mbalkarkaya5@gmail.com>
2025-04-10 18:12:39 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: 40 }}>
+      <h1>MRT LUXURY INVESTMENTS</h1>
+      <p>Welcome to your real estate investment hub.</p>
+    </div>
+  );
+}
